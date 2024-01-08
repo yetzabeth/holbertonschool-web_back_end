d6626532630cd8ae54546ddc698b94d8fa8e970c
yetzabeth <yetzaher@gmail.com>
2024-01-08 01:32:03 +0000

    tansk 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+This module contains the function to_str.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of the given float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
+
+
+if __name__ == "__main__":
+    # Test the function
+    pi_str = to_str(3.14)
+    print(pi_str == str(3.14))
+    print(to_str.__annotations__)
+    print("to_str(3.14) returns {} which is a {}".format(pi_str, type(pi_str)))

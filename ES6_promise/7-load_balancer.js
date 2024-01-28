5cf4f7959ec3007892025e0e923180555ebfd81c
yetzabeth <135637883+yetzabeth@users.noreply.github.com>
2024-01-28 04:37:01 +0000

    correction task 7

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,4 +1,4 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-    return Promise.race([chinaDownload, USDownload])
-      .then((val) => (val));
+  return Promise.race([chinaDownload, USDownload])
+    .then((val) => val);
 }

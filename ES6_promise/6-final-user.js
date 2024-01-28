9f6688d47952412ea2965f22b72d8e5345d1ed9e
yetzabeth <135637883+yetzabeth@users.noreply.github.com>
2024-01-28 04:31:17 +0000

    correction task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,9 +2,19 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ])
-    .then((results) => (results));
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => {
+      const [userResult, photoResult] = results;
+
+      const userPayload = userResult.status === 'fulfilled' ? userResult.value : null;
+      const photoPayload = photoResult.status === 'fulfilled' ? photoResult.value : null;
+
+      return {
+        user: userPayload,
+        photo: photoPayload,
+      };
+    });
 }

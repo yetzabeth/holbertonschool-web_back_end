d5a4302363335ea2d4f0e0a4b500e298d4cdea68
yetzabeth <135637883+yetzabeth@users.noreply.github.com>
2024-01-28 04:56:58 +0000

    task 9

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,12 +1,12 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
-    try {
-      queue.push(mathFunction());
-    } catch (error) {
-      queue.push(`Error: ${error.message}`);
-    } finally {
-      queue.push('Guardrail was processed');
-    }
-
-    return queue;
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
   }
+
+  return queue;
+}

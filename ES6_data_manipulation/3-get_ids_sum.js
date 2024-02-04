387b0348ae770d5da6f48102fb927f4522a35d23
yetzabeth <135637883+yetzabeth@users.noreply.github.com>
2024-02-04 11:18:48 +0000

    task 3-6

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,4 @@
+export default function getStudentIdsSum(stdList) {
+    const sum = stdList.reduce((acc, current) => acc + current.id, 0);
+    return sum;
+}
---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+    const studentsByCity = studentList.filter((student) => student.location === city);
+    const updatedStudents = studentsByCity.map((student) => {
+      const grade = newGrades.find((grade) => grade.studentId === student.id);
+      const updatedStudent = { ...student }; // Create a copy of the student object
+      if (grade) {
+        updatedStudent.grade = grade.grade;
+      } else {
+        updatedStudent.grade = 'N/A';
+      }
+      return updatedStudent;
+    });
+
+    return updatedStudents;
+}
